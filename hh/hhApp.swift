da601a9502a0e96eec7b851d850913ff0b0b2544
LidiaNKR <77931445+LidiaNKR@users.noreply.github.com>
2024-03-13 13:06:12 +0300

    Initial Commit

---
+++ hh/hhApp.swift
@@ -0,0 +1,17 @@
+//
+//  hhApp.swift
+//  hh
+//
+//  Created by Лидия Некрасова on 13.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct hhApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
